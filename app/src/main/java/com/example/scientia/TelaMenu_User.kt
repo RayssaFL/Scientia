10c75977d1425ee5ff54b267ad9e156a4aeb6429
PedroNatG <natan.v.r.guima@gmail.com>
2025-10-22 03:18:39 -0300

    Home_User(esta completamente bugada, mas eu subi pra gente ver amanha)

--- app/src/main/java/com/example/scientia/TelaMenu_User.kt
+++ app/src/main/java/com/example/scientia/TelaMenu_User.kt
@@ -5,16 +5,40 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class TelaMenu_User : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTelaMenu_User
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        binding = ActivityTelaMenu_User.inflate(layoutInflater)
         setContentView(R.layout.activity_tela_menu_user)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener{
+
+            when(it.itemId){
+                R.id.nav_home -> replaceFragment(Home())
+                R.id.nav_events -> replaceFragment(Eventos())ss
+                R.id.nav_contato -> replaceFragment(Contato())
+                R.id.nav_profile -> replaceFragment(Profile())
+
+                else ->{
+
+                }
+            }
+            true
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager= supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
